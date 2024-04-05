89ef65446c0fa078bc1f5f81209e85dd7c5d842b
Promesse RUKUNDO <130702756+Promesse7@users.noreply.github.com>
2024-04-04 20:57:17 -0700

    enter key compromised

--- Generative AI/gen.js
+++ Generative AI/gen.js
@@ -4,6 +4,24 @@ var answer ='';
 var answerLength;
 
 
+   queryInput.addEventListener("keypress", function(event) {
+    // Check for Enter key press
+    if (event.key === "Enter"){   
+        document.getElementById('answer').style.display = 'block';
+    let question = document.getElementById('queryInput').value;
+
+    answer = 'I cannot understand your question.';
+    if (question.length) {
+        answer = "VS Code uses TypeScript type declaration (typings) files (for example node.d.ts) to provide metadata to VS Code about the JavaScript based frameworks you are consuming in your application. Type declaration files are written in TypeScript so they can express the data types of parameters and functions, allowing VS Code to provide a rich IntelliSense experience. Thanks to a feature called Automatic Type Acquisition, you do not have to worry about downloading these type declaration files, VS Code will install them automatically for you"
+    }
+    answerLength = answer.length;
+//calling typing function
+    typing();
+    }
+    });
+  
+
+
 document.addEventListener('click',function(e){
     if (e.target.id =='btn') {
         document.getElementById('answer').style.display = 'block';
