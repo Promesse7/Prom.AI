90e6a1a2b3081662c8e8ca66d96caddf8e4fea6e
Promesse RUKUNDO <130702756+Promesse7@users.noreply.github.com>
2024-04-07 05:44:46 -0700

    creating chit-chat

--- Generative AI/gen.js
+++ Generative AI/gen.js
@@ -49,3 +49,72 @@ if(i < answerLength){
     setTimeout(typing, speed);
     }
 }
+
+
+
+//starting of my chit-chat
+
+const johnSelectorBtn = document.querySelector('#john-selector')
+const janeSelectorBtn = document.querySelector('#jane-selector')
+const chatHeader = document.querySelector('.chat-header')
+const chatMessages = document.querySelector('.chat-messages')
+const chatInputForm = document.querySelector('.chat-input-form')
+const chatInput = document.querySelector('.chat-input')
+const clearChatBtn = document.querySelector('.clear-chat-button') 
+
+
+const messages = JSON.parse(localStorage.getItem('messages')) || []
+
+const createChatMessageElement = (message) => `
+      <div class="response" id="answer"></div>
+`
+
+window.onload = () => {
+    messages.forEach((message) => {
+        chatMessages.innerHTML += createChatMessageElement(message)
+    });
+}
+
+let messageSender = 'John'
+const updateMessageSender = (name) =>{
+    messageSender = name
+    chatHeader.innerText = `${messageSender} chatting...` 
+    chatInput.placeholder = `Type here, ${messageSender}...`
+
+    if (name === 'John') {
+        johnSelectorBtn.classList.add('active-person')
+        janeSelectorBtn.classList.remove('active-person')
+    }
+    if (name === 'Jane') {
+        johnSelectorBtn.classList.remove('active-person')
+        janeSelectorBtn.classList.add('active-person')
+    }
+    chatInput.focus()
+}
+
+johnSelectorBtn.onclick = () => updateMessageSender('John')
+janeSelectorBtn.onclick = () => updateMessageSender('Jane') 
+
+const sendMessage = (e) =>{
+    e.preventDefault();
+
+    const timestamp = new Date().toLocaleString('en-US', { hour:'numeric', minute:'numeric', hour12:true })
+    const message = {
+        send: messageSender,
+        text:chatInput.value,
+        timestamp,
+    }
+    chatMessages.innerHTML += createChatMessageElement(message)
+    
+    messages.push(message)
+    localStorage.setItem('messages', JSON.stringify(messages))
+    chatInputForm.reset()
+    chatMessages.scrollTop = chatMessages.scrollHeight
+}
+
+chatInputForm.addEventListener('submit', sendMessage)
+
+clearChatBtn.addEventListener('click', () =>{
+    localStorage.clear()
+    chatMessages.innerHTML = ''
+})
